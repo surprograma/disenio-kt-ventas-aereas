ae50a3e2db8fba7f8b70ef58fbd8c624decad9db
Federico Aloi <federico.aloi@gmail.com>
2021-09-07 09:25:29 -0300

    Agregado código inicial

--- src/main/kotlin/ar/edu/unahur/obj2/ventasAereas/Vuelo.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/ventasAereas/Vuelo.kt
@@ -1,3 +1,14 @@
 package ar.edu.unahur.obj2.ventasAereas
 
-// Nada por aquí...
+import java.time.LocalDate
+
+class Avion(val cantidadMaximaAsientos: Int, val peso: Int)
+
+class Ciudad(val nombre: String)
+
+abstract class Vuelo(fecha: LocalDate, avion: Avion, origen: Ciudad, destino: Ciudad, precioEstandar: Double) {
+  fun asientosLibres() = asientosDisponibles() - asientosOcupados()
+
+  abstract fun asientosDisponibles(): Int;
+  abstract fun asientosOcupados(): Int;
+}
